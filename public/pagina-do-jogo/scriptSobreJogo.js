4a89f7964e23d6e84631746e0f1e6b70c779c0f8
PedroSchneider1 <ps.pedroschneider@gmail.com>
2024-05-02 21:39:43 -0300

    adicionado texto e animação

--- public/pagina-do-jogo/scriptSobreJogo.js
+++ public/pagina-do-jogo/scriptSobreJogo.js
@@ -1,3 +1,37 @@
+// FUNÇÃO busAnimation
+var bus = document.getElementById("busDiv").getElementsByClassName("bus")[0]; // Pega apenas o GIF do ônibus
+var busDiv = document.getElementById("busDiv");
+var change = -200; // Posição do ônibus (tem 200 de width, logo ele começa fora da tela)
+var pageLimit = busDiv.offsetWidth // Define o comprimento total da div 'busDiv' (limite da página)
+
+if (pageLimit % 2 != 0){ // Transforma o limite da página em um número par
+  pageLimit += 1;
+}
+
+bus.addEventListener("click", function(){
+  change += 2;
+});
+
+function busAnimation(){
+  bus.style.right = change+"px"; // Muda a propriedade 'right' no css de 'busDiv'
+
+  if(change == (pageLimit-200)){
+    change = (pageLimit-200);
+  }
+  else{
+    change += 2;
+    if(change == pageLimit){
+      change = -200;
+    }
+  }
+  requestAnimationFrame(busAnimation);
+
+  
+}
+
+busAnimation();
+
+// FUNÇÃO fixNav
 // Quando o usuário desce a página, executa a função 'fixNav()'
 window.onscroll = function() {fixNav()};
 
