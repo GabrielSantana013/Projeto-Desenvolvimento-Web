772befdc95e1fe4f97eab62631ccad4d070bc540
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-05-17 12:21:43 -0300

    Finalizado a página sobre nós com modo escuro

--- public/pagina-sobre-nos/scriptSobreNos.js
+++ public/pagina-sobre-nos/scriptSobreNos.js
@@ -1,5 +1,7 @@
-var botao = document.getElementById("segredo");
-var divImg = document.querySelector(".contImg");
+//Div Secreta
+
+let botao = document.getElementById("segredo");
+let divImg = document.querySelector(".contImg");
 
 botao.addEventListener("click", function(){
 
@@ -13,3 +15,25 @@ botao.addEventListener("click", function(){
     }
 
 });
+
+    //Modo escuro
+
+    let trilho = document.getElementById("trilho");
+    let body = document.querySelector('body');
+    let header = document.querySelector('header');
+    let topoDoSite = document.querySelector(".topoDoSite");
+    let sobreGabriel = document.querySelector(".sobreGabriel");
+    let sobrePedro = document.querySelector(".sobrePedro");
+    let linhaRodape2 = document.querySelector(".linhaRodape2");
+    let contImg = document.querySelector('.contImg');
+
+    trilho.addEventListener('click', function(){
+        trilho.classList.toggle('dark'); //assim que clicar ele muda pra dark
+        body.classList.toggle('dark');
+        header.classList.toggle('dark');
+        topoDoSite.classList.toggle('dark');
+        sobreGabriel.classList.toggle('dark');
+        sobrePedro.classList.toggle('dark');
+        linhaRodape2.classList.toggle('dark');
+        contImg.classList.toggle('dark');
+    });
